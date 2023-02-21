c1ff5cb04a2612612406ecd94e0e1a9c746fd506
18493 <1849325557@qq.com>
2023-02-21 09:35:19 +0800

    加载数据集

---
+++ train_dev_3070.py
@@ -0,0 +1,230 @@
+# -*- coding: utf-8 -*-
+import argparse
+import datetime
+import os
+import time
+from torch.utils.tensorboard import SummaryWriter
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import albumentations as albu
+import torch
+import segmentation_models_pytorch as smp
+from segmentation_models_pytorch.utils import *
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset as BaseDataset
+
+from tools.augmentation import *
+from tools.datasets_VOC import Dataset
+import yaml
+
+class Trainer():
+    def __init__(self,args):
+        self.args = args
+
+        with open(args.model, 'r', encoding='utf-8') as f:
+            yamlresult = yaml.load(f.read(), Loader=yaml.FullLoader)
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.dir = args.data_path
+        self.encoder = yamlresult['encoder']
+        self.encoder_weights = yamlresult['encoder_weights']
+        self.classes = yamlresult['classes']
+        self.activation = yamlresult['activation']
+        self.model_name = yamlresult['model_name']
+
+        self.model = self.create_model()
+        self.preprocessing_fn = smp.encoders.get_preprocessing_fn(self.encoder, self.encoder_weights)
+        self.loss = losses.DiceLoss()
+        self.metrics = [metrics.IoU(threshold=0.5),metrics.Fscore(beta=1,threshold=0.5),metrics.Accuracy(threshold=0.5)]
+        self.optimizer = torch.optim.Adam([dict(params=self.model.parameters(), lr=args.lr),])
+
+        self.batch_size = args.batch_size
+        self.epochs = args.epochs
+        self.num_workers = args.num_workers
+
+    def create_model(self):
+        # create segmentation model with pretrained encoder
+        if self.model_name == 'unet':
+            model = smp.Unet(
+                encoder_name=self.encoder,
+                encoder_weights=self.encoder_weights,
+                classes=len(self.classes),
+                activation=self.activation,
+            )
+        return model
+
+    def dataload(self):
+        # 训练集
+        x_train_dir = os.path.join(self.dir, 'train')
+        y_train_dir = os.path.join(self.dir, 'trainannot')
+
+        # 验证集
+        x_valid_dir = os.path.join(self.dir, 'val')
+        y_valid_dir = os.path.join(self.dir, 'valannot')
+
+        # 创建训练集和验证集
+        train_dataset = Dataset(
+            x_train_dir,
+            y_train_dir,
+            augmentation=get_training_augmentation(base_size=args.base_size, crop_size=args.crop_size),
+            preprocessing=get_preprocessing(self.preprocessing_fn),
+            classes=self.classes,
+        )
+
+        valid_dataset = Dataset(
+            x_valid_dir,
+            y_valid_dir,
+            augmentation=get_validation_augmentation(base_size=args.base_size),
+            preprocessing=get_preprocessing(self.preprocessing_fn),
+            classes=self.classes,
+        )
+
+        # 创建训练集和验证集的数据加载器
+        train_loader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
+        valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=self.num_workers)
+
+        return train_loader,valid_loader
+    def dataload_VOC(self):
+        # 训练集
+        x_train_dir = os.path.join(self.dir, 'VOC2007/JPEGImages')
+        y_train_dir = os.path.join(self.dir, 'VOC2007/SegmentationClass')
+
+        # 验证集
+        x_valid_dir = os.path.join(self.dir, 'VOC2007/JPEGImages')
+        y_valid_dir = os.path.join(self.dir, 'VOC2007/SegmentationClass')
+
+        # 创建训练集和验证集
+        train_dataset = Dataset(
+            x_train_dir,
+            y_train_dir,
+            augmentation=get_training_augmentation(base_size=args.base_size, crop_size=args.crop_size),
+            preprocessing=get_preprocessing(self.preprocessing_fn),
+            classes=self.classes,
+        )
+
+        valid_dataset = Dataset(
+            x_valid_dir,
+            y_valid_dir,
+            augmentation=get_validation_augmentation(base_size=args.base_size),
+            preprocessing=get_preprocessing(self.preprocessing_fn),
+            classes=self.classes,
+        )
+
+        # 创建训练集和验证集的数据加载器
+        train_loader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
+        valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=self.num_workers)
+
+        return train_loader,valid_loader
+
+    def train_one_epoch(self):
+        train_epoch = train.TrainEpoch(
+            self.model,
+            loss=self.loss,
+            metrics=self.metrics,
+            optimizer=self.optimizer,
+            device=self.device,
+            verbose=True,
+        )
+        return train_epoch
+
+    def valid_one_epoch(self):
+        valid_epoch = train.ValidEpoch(
+            self.model,
+            loss=self.loss,
+            metrics=self.metrics,
+            device=self.device,
+            verbose=True,
+        )
+        return valid_epoch
+
+    # 创建log文件夹
+    def create_folder(self):
+        # 用来保存训练以及验证过程中信息
+        if not os.path.exists("logs"):
+            os.mkdir("logs")
+        # 创建时间+模型名文件夹
+        time_str = datetime.datetime.now().strftime("%m-%d %H_%M_%S-")
+        log_dir = os.path.join("logs", time_str + self.model_name)
+        if not os.path.exists(log_dir):
+            os.mkdir(log_dir)
+        self.results_file = log_dir + "/{}_results{}.txt".format(self.model_name, time_str)
+        # 实例化tensborad
+        self.tb = SummaryWriter(log_dir=log_dir)
+        return log_dir
+
+    def run(self):
+        log_dir=self.create_folder()
+        # 创建训练集和验证集的数据加载器
+        train_loader,valid_loader = self.dataload()
+
+        # 创建一个简单的循环，用于迭代数据样本
+        train_epoch2 = self.train_one_epoch()
+        valid_epoch2 = self.valid_one_epoch()
+
+        max_score = 0
+        start_time = time.time()
+        for i in range(0, self.epochs):
+            print('\nEpoch: {}'.format(i))
+            train_logs = train_epoch2.run(train_loader)
+            valid_logs = valid_epoch2.run(valid_loader)
+            # 使用tb保存训练过程中的信息
+            self.tb.add_scalar('loss', train_logs['dice_loss'], i)
+            self.tb.add_scalar('iou_score', train_logs['iou_score'], i)
+            self.tb.add_scalar('lr', self.optimizer.param_groups[0]['lr'], i)
+            # 保存训练过程中的信息
+            with open(self.results_file, "a") as f:
+                f.write("Epoch: {} - \n".format(i))
+                f.write("Train: {} - \n".format(train_logs))
+                f.write("Valid: {} - \n".format(valid_logs))
+
+            # do something (save model, change lr, etc.)
+            if max_score < valid_logs['iou_score']:
+                max_score = valid_logs['iou_score']
+                torch.save(self.model, './best_model_mine.pth')
+                print('Model saved!')
+            if i == 5:
+                self.optimizer.param_groups[0]['lr'] = 1e-5
+                print('Decrease decoder learning rate to 1e-5!')
+
+        total_time = time.time() - start_time
+        total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+        print("training total_time: {}".format(total_time_str))
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="pytorch segnets training")
+    # 主要
+    parser.add_argument("--model", default="unet.yaml", type=str, help="选择模型",
+                        choices=["unet","deeplabv3"])
+    parser.add_argument("--data-path", default=r'xxxxx', help="VOCdevkit 路径")
+    parser.add_argument("--batch-size", default=2, type=int,help="分块大小")
+    parser.add_argument("--base-size", default=[512,512], type=int,help="图片缩放大小")
+    parser.add_argument("--crop-size", default=[512,512], type=int,help="图片裁剪大小")
+    parser.add_argument("--epochs", default=20, type=int, metavar="N",help="训练轮数")
+    parser.add_argument("--num-workers", default=0, type=int, help="数据加载器的线程数")
+    parser.add_argument('--lr', default=0.0001, type=float, help='初始学习率')
+
+    # 暂无
+    parser.add_argument("--pretrained", default=r"", type=str, help="权重位置的路径")
+    parser.add_argument('--resume', default=r"", help='继续训练的权重位置的路径')
+    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',help='动量')
+    parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
+                        metavar='W', help='权重衰减',dest='weight_decay')
+    parser.add_argument('--optimizer', default='SGD', type=str, choices=['SGD', 'Adam', 'AdamW'], help='优化器')
+    # 其他
+    parser.add_argument('--open-tb', default=False, type=bool, help='使用tensorboard保存网络结构')
+
+    args = parser.parse_args()
+
+    return args
+
+# $# 创建模型并训练
+# ---------------------------------------------------------------
+if __name__ == '__main__':
+
+    # 数据集所在的目录
+    args = parse_args()
+    trainer = Trainer(args)
+    trainer.run()
---
+++ voc.py
@@ -0,0 +1,156 @@
+import os
+import cv2
+import numpy as np
+import json
+import random
+import shutil
+from tqdm import tqdm
+
+'''
+制作一个只包含分类标注的标签图像，假如我们分类的标签为cat和dog，那么该标签图像中，Background为0，cat为1，dog为2。
+我们首先要创建一个和原图大小一致的空白图像，该图像所有像素都是0，这表示在该图像中所有的内容都是Background。
+然后根据标签对应的区域使用与之对应的类别索引来填充该图像，也就是说，将cat对应的区域用1填充，dog对应的区域用2填充。
+特别注意的是，一定要有Background这一项且一定要放在index为0的位置。
+'''
+def make_mask(dir,classes):
+    os.makedirs(os.path.join(dir,'masks'), exist_ok=True)
+    # 分类标签，一定要包含'Background'且必须放在最前面
+    category_types = classes
+    # 将图片标注json文件批量生成训练所需的标签图像png
+    imgpath_list = os.listdir(os.path.join(dir,'img_data'))
+    for img_path in imgpath_list:
+        img_name = img_path.split('.')[0]
+        img = cv2.imread(os.path.join(dir,'img_data', img_path))
+        h, w = img.shape[:2]
+        # 创建一个大小和原图相同的空白图像
+        mask = np.zeros([h, w, 1], np.uint8)
+
+        with open(os.path.join(dir,'data_annotated',img_name+'.json'), encoding='utf-8') as f:
+            label = json.load(f)
+
+        masks = label['shapes']
+        for shape in masks:
+            category = shape['label']
+            points = shape['points']
+            # 将图像标记填充至空白图像
+            points_array = np.array(points, dtype=np.int32)
+            mask = cv2.fillPoly(mask, [points_array], category_types.index(category))
+
+        # 生成的标注图像必须为png格式
+        cv2.imwrite(os.path.join(dir,'masks',img_name+'.png'), mask)
+        print('mask image saved to: ', os.path.join(dir,'masks',img_name+'.png'))
+    print('mask images generated successfully!')
+# 之后完善可视化函数
+def visualization(dir,classes):
+    category_types = classes
+    # 将图片标注json文件批量生成训练所需的标签图像png
+    imgpath_list = os.listdir(os.path.join(dir,'img_data'))
+    for img_path in imgpath_list:
+        img_name = img_path.split('.')[0]
+        img = cv2.imread(os.path.join(dir,'img_data', img_path))
+        h, w = img.shape[:2]
+        # 创建一个大小和原图相同的空白图像
+        mask = np.zeros([h, w, 1], np.uint8)
+
+        with open(os.path.join(dir,'data_annotated',img_name+'.json'), encoding='utf-8') as f:
+            label = json.load(f)
+
+        masks = label['shapes']
+        for shape in masks:
+            category = shape['label']
+            points = shape['points']
+
+            # 将图像标记填充至空白图像
+            points_array = np.array(points, dtype=np.int32)
+            # mask = cv2.fillPoly(mask, [points_array], category_types.index(category))
+
+            if category == 'Gingerbread':
+                # 调试时将某种标注的填充颜色改为255，便于查看用，实际时不需进行该操作
+                mask = cv2.fillPoly(mask, [points_array], 125)
+            elif category == 'end_skew':
+                mask = cv2.fillPoly(mask, [points_array], 255)
+            else:
+                mask = cv2.fillPoly(mask, [points_array], category_types.index(category))
+
+        cv2.imshow('mask', mask)
+        cv2.waitKey(0)
+
+
+def split_data(dir,classes,train_percent=0.7,val_percent=0.2,test_percent=0.1):
+    '''
+    ├── data(按照7:2:1比例划分)
+    │   ├── train 存放用于训练的图片
+    │   ├── trainannot 存放用于训练的图片标注
+    │   ├── val 存放用于验证的图片
+    │   ├── valannot 存放用于验证的图片标注
+    │   ├── test 存放用于测试的图片
+    │   ├── testannot 存放用于测试的图片标注
+    '''
+
+    # 创建数据集文件夹
+    dirpath_list = ['data/train', 'data/trainannot', 'data/val', 'data/valannot', 'data/test', 'data/testannot']
+    for dirpath in dirpath_list:
+        # if os.path.exists(dirpath):
+        #     shutil.rmtree(dirpath)   # 删除原有的文件夹
+        #     os.makedirs(dirpath)   # 创建文件夹
+        # elif not os.path.exists(dirpath):
+        #     os.makedirs(dirpath)
+        os.makedirs(os.path.join(dir,dirpath), exist_ok=True)
+
+
+    # # 训练集、验证集、测试集所占比例
+    # train_percent = 0.7
+    # val_percent = 0.2
+    # test_percent = 0.1
+
+    # 数据集原始图片所存放的文件夹，必须为png文件
+    imagefilepath = os.path.join(dir,'img_data')
+    total_img = os.listdir(imagefilepath)
+    # 所有数据集的图片名列表
+    total_name_list = [row.split('.')[0] for row in total_img]
+    num = len(total_name_list)
+    num_list = range(num)
+    # 训练集、验证集、测试集所包含的图片数目
+    train_tol = int(num * train_percent)
+    val_tol = int(num * val_percent)
+    test_tol = int(num * test_percent)
+
+    # 训练集在total_name_list中的index
+    train_numlist = random.sample(num_list, train_tol)
+    # 验证集在total_name_list中的index
+    val_test_numlist = list(set(num_list) - set(train_numlist))
+    val_numlist = random.sample(val_test_numlist, val_tol)
+    # 测试集在total_name_list中的index
+    test_numlist = list(set(val_test_numlist) - set(val_numlist))
+
+    # 将数据集和标签图片安装分类情况依次复制到对应的文件夹
+    for i in tqdm(train_numlist,desc='train'):
+        img_path = os.path.join(dir,'img_data',total_name_list[i]+'.jpg')
+        new_path = os.path.join(dir,'data/train',total_name_list[i]+'.png')
+        shutil.copy(img_path, new_path)
+        img_path = os.path.join(dir,'masks',total_name_list[i]+'.png')
+        new_path = os.path.join(dir,'data/trainannot',total_name_list[i]+'.png')
+        shutil.copy(img_path, new_path)
+    for i in tqdm(val_numlist,desc='val'):
+        img_path = os.path.join(dir,'img_data',total_name_list[i]+'.jpg')
+        new_path = os.path.join(dir,'data/val',total_name_list[i]+'.png')
+        shutil.copy(img_path, new_path)
+        img_path = os.path.join(dir,'masks',total_name_list[i]+'.png')
+        new_path = os.path.join(dir,'data/valannot',total_name_list[i]+'.png')
+        shutil.copy(img_path, new_path)
+    for i in tqdm(test_numlist,desc='test'):
+        img_path = os.path.join(dir,'img_data',total_name_list[i]+'.jpg')
+        new_path = os.path.join(dir,'data/test',total_name_list[i]+'.png')
+        shutil.copy(img_path, new_path)
+        img_path = os.path.join(dir,'masks',total_name_list[i]+'.png')
+        new_path = os.path.join(dir,'data/testannot',total_name_list[i]+'.png')
+        shutil.copy(img_path, new_path)
+    print('数据集划分完成！')
+
+if __name__ == '__main__':
+    dir = r'C:\Users\18493\Desktop\end_skew'
+    classes = ['Background', 'end_skew']
+    # # make_mask(dir,classes)
+    # split_data(dir,classes,train_percent=0.7,val_percent=0.2,test_percent=0.1)
+
+    visualization(dir,classes)
